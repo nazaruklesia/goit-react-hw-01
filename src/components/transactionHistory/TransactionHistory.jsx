8b2f877c7a977d5161b80ede24efc1854ff405e8
Lesia_Nazaruk <nazarullesia@gmail.com>
2025-01-16 00:33:52 +0200

    add transactions

--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -1,26 +1,25 @@
-const TransactionHistory = ({ type, amount, currency }) => {
+import styles from "./TransactionHistory.module.css";
+
+const TransactionHistory = ({ items }) => {
   return (
-    <div>
-      <table>
+    <div className={styles.transactions}>
+      <table className={styles.table}>
         <thead>
           <tr>
-            <th>{type}</th>
-            <th>{amount}</th>
-            <th>{currency}</th>
+            <th className={styles.title}>Type</th>
+            <th className={styles.title}>Amount</th>
+            <th className={styles.title}>Currency</th>
           </tr>
         </thead>
 
-        <tbody>
-          <tr>
-            <td>Invoice</td>
-            <td>125</td>
-            <td>USD</td>
-          </tr>
-          <tr>
-            <td>Withdrawal</td>
-            <td>85</td>
-            <td>USD</td>
-          </tr>
+        <tbody className={styles.tbody}>
+          {items.map(({ id, type, amount, currency }) => (
+            <tr key={id}>
+              <td className={styles.box}>{type}</td>
+              <td className={styles.box}>{amount}</td>
+              <td className={styles.box}>{currency}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
